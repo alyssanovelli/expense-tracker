fcde75300501a905024507bf2d225d45b8609c97
alyssanovelli <anovelli028@gmail.com>
2026-08-07 09:19:20 -0400

    File structure setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Dashboard from './pages/Dashboard.jsx'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: 'dashboard',        
+        element: <Dashboard />,
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
