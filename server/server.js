391651556324a01e23d930257cf475143520c580
alyssanovelli <anovelli028@gmail.com>
2026-08-12 15:14:21 -0400

    Implement and test get and post transactions

--- server/server.js
+++ server/server.js
@@ -2,10 +2,25 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Expense Tracker server is working!");
 });
-
+app.get("/api/transactions", (req, res) => {
+    const transactions = [
+        {
+            name: "Coffee",
+            amount: 6.40,
+            type: "expense",
+        },
+        {
+            name: "Salary",
+            amount: 2000,
+            type: "income",
+        },
+    ];
+    res.json(transactions);
+});
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
