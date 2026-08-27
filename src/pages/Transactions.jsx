f648820b7eb7feb4e0dbe2700ddc6a1ab76ff75f
alyssanovelli <anovelli028@gmail.com>
2026-08-27 09:43:10 -0400

    Fix calendar bug

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -214,14 +214,21 @@ function Transactions() {
             <strong>{transaction.name}</strong>
 
             <span>
-                {new Date(transaction.date).toLocaleDateString(
-                    "en-US",
-                    {
+                {(() => {
+                    const [year, month, day] = transaction.date
+                        .slice(0, 10)
+                        .split("-");
+
+                    return new Date(
+                        Number(year),
+                        Number(month) - 1,
+                        Number(day)
+                    ).toLocaleDateString("en-US", {
                         year: "numeric",
                         month: "short",
                         day: "numeric",
-                    }
-                )}
+                    });
+                })()}
             </span>
         </div>
 
